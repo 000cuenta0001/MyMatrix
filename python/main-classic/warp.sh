fe515ce30a889c09e5ecb1277269f0fdac906b88
yosecuales <000cuenta0001@gmail.com>
2024-03-25 16:40:45 +0100

    h

--- python/main-classic/warp.sh
+++ python/main-classic/warp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DOCKER="/storage/.kodi/addons/service.system.docker/bin/docker"
 
 # Definir el directorio de trabajo
 workdirectory="/storage/.kodi"
@@ -30,40 +31,40 @@ mkdir wgcf
 echo "5. Creado directorio 'wgcf'"
 
 # Verificar y eliminar el contenedor wgcfconf si existe
-if docker ps -a --format '{{.Names}}' | grep -q '^wgcfconf$'; then
-    docker stop wgcfconf
-    docker rm wgcfconf
+if $DOCKER ps -a --format '{{.Names}}' | grep -q '^wgcfconf$'; then
+    $DOCKER stop wgcfconf
+    $DOCKER rm wgcfconf
     echo "6. Detenido y eliminado contenedor 'wgcfconf'"
 fi
 
 # Ejecutar el primer contenedor en segundo plano
-container_id=$(docker run -d --name wgcfconf --sysctl net.ipv6.conf.all.disable_ipv6=0 --privileged --cap-add net_admin -v /lib/modules:/lib/modules -v $(pwd)/wgcf:/wgcf neilpang/wgcf-docker:alpine -4)
+container_id=$($DOCKER run -d --name wgcfconf --sysctl net.ipv6.conf.all.disable_ipv6=0 --privileged --cap-add net_admin -v /lib/modules:/lib/modules -v $(pwd)/wgcf:/wgcf neilpang/wgcf-docker:alpine -4)
 
 # Esperar a que el contenedor termine (puede ajustar el tiempo según sus necesidades)
-docker wait "$container_id"
+$DOCKER wait "$container_id"
 
 # Al salir del contenedor, eliminar el contenedor
-docker rm "$container_id"
+$DOCKER rm "$container_id"
 echo "7. Ejecutado contenedor 'wgcfconf' y esperado a su término"
 
 # Verificar y eliminar el contenedor wgcf si existe
-if docker ps -a --format '{{.Names}}' | grep -q '^wgcf$'; then
-    docker stop wgcf
-    docker rm wgcf
+if $DOCKER ps -a --format '{{.Names}}' | grep -q '^wgcf$'; then
+    $DOCKER stop wgcf
+    $DOCKER rm wgcf
     echo "8. Detenido y eliminado contenedor 'wgcf'"
 fi
 
 # Verificar y eliminar el contenedor ace_proxy si existe
-if docker ps -a --format '{{.Names}}' | grep -q '^ace_proxy$'; then
-    docker stop ace_proxy
-    docker rm ace_proxy
+if $DOCKER ps -a --format '{{.Names}}' | grep -q '^ace_proxy$'; then
+    $DOCKER stop ace_proxy
+    $DOCKER rm ace_proxy
     echo "9. Detenido y eliminado contenedor 'ace_proxy'"
 fi
 
 # Levantar contenedor final con configuración wgcf
-docker run -d --name wgcf --privileged --cap-add net_admin --sysctl net.ipv6.conf.all.disable_ipv6=0 -v /lib/modules:/lib/modules -v $(pwd)/wgcf:/wgcf -p 8000:8000 -p 8621:8621 -p 6878:6878 --restart unless-stopped neilpang/wgcf-docker sh -c "sed -i -e '/^Address =/n; /^Address =/d' -e 's/AllowedIPs = 0.0.0.0\\/0/AllowedIPs = 0.0.0.0\\/1/' -e 's/AllowedIPs = ::\\/0/AllowedIPs = ::\\/1/' -e '/^DNS =/d' -e '/^MTU =/d' /wgcf/wgcf-profile.conf && cp /wgcf/wgcf-profile.conf /etc/wireguard/wgcf.conf && wg-quick up wgcf && tail -f /dev/null"
+$DOCKER run -d --name wgcf --privileged --cap-add net_admin --sysctl net.ipv6.conf.all.disable_ipv6=0 -v /lib/modules:/lib/modules -v $(pwd)/wgcf:/wgcf -p 8000:8000 -p 8621:8621 -p 6878:6878 --restart unless-stopped neilpang/wgcf-docker sh -c "sed -i -e '/^Address =/n; /^Address =/d' -e 's/AllowedIPs = 0.0.0.0\\/0/AllowedIPs = 0.0.0.0\\/1/' -e 's/AllowedIPs = ::\\/0/AllowedIPs = ::\\/1/' -e '/^DNS =/d' -e '/^MTU =/d' /wgcf/wgcf-profile.conf && cp /wgcf/wgcf-profile.conf /etc/wireguard/wgcf.conf && wg-quick up wgcf && tail -f /dev/null"
 echo "10. Levantado contenedor final 'wgcf'"
 
 # Levantar contenedor aceproxy
-docker run -d --name ace_proxy --network container:wgcf --privileged --restart unless-stopped ef1f/raspberry_ace_proxy:latest
+$DOCKER run -d --name ace_proxy --network container:wgcf --privileged --restart unless-stopped ef1f/raspberry_ace_proxy:latest
 echo "11. Levantado contenedor 'ace_proxy'"
